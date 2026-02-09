693e55b1f3cb7f92eb50b9dd76bd2b6144ff9709
Dhruv Singhania <dhruv.s0611@gmail.com>
2026-02-10 00:03:59 +0530

    created activeTabSwitcher component

--- frontend/src/components/ActiveTabSwitcher.jsx
+++ frontend/src/components/ActiveTabSwitcher.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useChatStore } from '../store/chatStore'
 
 const ActiveTabSwitcher = () => {
+  const { activeTab, setActiveTab } = useChatStore();
+
   return (
-    <div>
-      
+    <div className="tabs tabs-box bg-transparent p-2 m-2 flex justify-around items-center">
+
+      <button
+        onClick={() => setActiveTab("chats")}
+        className={`tab transition-all duration-300 rounded-lg
+          ${activeTab === "chats"
+            ? "bg-cyan-500/20 text-cyan-400 px-8 scale-105"
+            : "text-slate-400 px-4"
+          }`}
+      >
+        Chats
+      </button>
+
+      <button
+        onClick={() => setActiveTab("contacts")}
+        className={`tab transition-all duration-300 rounded-lg
+          ${activeTab === "contacts"
+            ? "bg-cyan-500/20 text-cyan-400 px-8 scale-105"
+            : "text-slate-400 px-4"
+          }`}
+      >
+        Contacts
+      </button>
+
     </div>
   )
 }
