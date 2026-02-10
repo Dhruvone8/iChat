1630fe5c8eb4ff044e941fce70e14c60bfa4118c
Dhruv Singhania <dhruv.s0611@gmail.com>
2026-02-10 23:48:03 +0530

    Built Chat Header

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChatStore } from '../store/ChatStore'
+import { useAuthStore } from '../store/AuthStore'
+import ChatHeader from './ChatHeader'
 
 const ChatContainer = () => {
+
+  const { selectedUser, getMessagesByUserId, messages } = useChatStore()
+  const { authUser } = useAuthStore()
+
+  useEffect(() => {
+    getMessagesByUserId(selectedUser._id)
+  }, [selectedUser, getMessagesByUserId])
+
   return (
-    <div>
-        
-    </div>
+    <>
+    <ChatHeader />
+    </>
   )
 }
 
