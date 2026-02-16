2f42ff75e87bd63542c97fcfc512105004a5890a
Dhruv Singhania <dhruv.s0611@gmail.com>
2026-02-16 22:15:07 +0530

    Implemented Online Users Functionality

--- frontend/src/components/ChatsList.jsx
+++ frontend/src/components/ChatsList.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react'
 import { useChatStore } from '../store/ChatStore'
 import UsersLoadingSkeleton from './UsersLoadingSkeleton'
 import NoChatsFound from './NoChatsFound'
+import { useAuthStore } from "../store/AuthStore"
 
 const ChatsList = () => {
   const { getMyChatPartners, chats, isUsersLoading, setSelectedUser } = useChatStore();
+  const { onlineUsers } = useAuthStore();
 
   useEffect(() => {
     getMyChatPartners()
@@ -19,7 +21,7 @@ const ChatsList = () => {
         <div key={chat._id} className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
           onClick={() => setSelectedUser(chat)}>
           <div className="flex items-center gap-3">
-            <div className={`avatar avatar-online`}>
+            <div className={`avatar ${onlineUsers.includes(chat._id) ? "avatar-online" : "avatar-offline"}`}>
               <div className="size-12 rounded-full">
                 <img src={chat.profilePic || "/avatar.png"} alt={chat.fullName} />
               </div>
